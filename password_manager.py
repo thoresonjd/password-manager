4a6d0295ee210a616d40d8fae5d2a365c16cfab6
thoresonjd <thoresonjd@gmail.com>
2023-11-22 20:33:31 -0800

    Can choose to hash with SHA-256 or SHA-512

--- password_manager.py
+++ password_manager.py
@@ -7,18 +7,37 @@ Date: 22 November 2023
 
 from typing import Any
 from argparse import ArgumentParser
-from hashlib import sha256 as sha
+from hashlib import sha256, sha512
 from base64 import b64encode
 
 def parse_args() -> tuple:
     """Parses command line arguments."""
     
     arg_parser = ArgumentParser()
-    arg_parser.add_argument('--service', type=str, help='The service in which to generate a password for')
-    arg_parser.add_argument('--secret', type=str, help='A secret phrase')
-    arg_parser.add_argument('--iteration', type=int, help='The number of times this password has been generated')
+    arg_parser.add_argument(
+        '--algorithm',
+        type=int,
+        choices=[256, 512],
+        default=256,
+        help='Which SHA-2 algorithm to use'
+    )
+    arg_parser.add_argument(
+        'service',
+        type=str,
+        help='The service in which to generate a password for'
+    )
+    arg_parser.add_argument(
+        'secret',
+        type=str, 
+        help='A secret phrase'
+    )
+    arg_parser.add_argument(
+        'iteration',
+        type=int,
+        help='The number of times this password has been generated'
+    )
     args = arg_parser.parse_args()
-    return args.service, args.secret, args.iteration
+    return args.algorithm, args.service, args.secret, args.iteration
 
 def encode(data: Any) -> bytes:
     """Encodes data to base 64."""
@@ -27,21 +46,34 @@ def encode(data: Any) -> bytes:
     data_utf8 = data_str.encode('UTF-8')
     return b64encode(data_utf8)
 
-def hash(data: Any) -> str:
-    """Hashes encoded data."""
+def hash(algorithm: int, data: Any) -> str:
+    """
+    Hashes encoded data.
+    :param algorithm: Which SHA-2 algorithm to use
+    :param data: The data to hash
+    :return: The hexadecimal string representatin of the hashed data
+    """
 
+    sha = sha512 if algorithm == 512 else sha256
     return sha(encode(data)).hexdigest()
 
-def generate_password(service: str, secret: str, iteration: int) -> str:
+def generate_password(
+    algorithm: int, service: str, secret: str, iteration: int
+) -> str:
     """
     Generates a password.
+    :param algorithm: Which SHA-2 algorithm to use
     :param service: The service in which to generate a password for
     :param secret: A secret phrase
     :param iteration: The number of times this password has been generated
     :return: A generated password
     """
 
-    return hash(''.join([hash(service), hash(secret), hash(iteration)]))
+    service_h = hash(algorithm, service)
+    secret_h = hash(algorithm, secret)
+    iteration_h = hash(algorithm, iteration)
+    composite = ''.join([service_h, secret_h, iteration_h])
+    return hash(algorithm, composite)
 
 def main() -> None:
     """Runs the password manager program."""

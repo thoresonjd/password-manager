e9d1cb958519a5f390d3c5af770d2231df5367a9
thoresonjd <thoresonjd@gmail.com>
2023-11-23 00:05:22 -0800

    Special characters inserted pseudorandomly into the password

--- password_manager.py
+++ password_manager.py
@@ -9,6 +9,7 @@ from typing import Any
 from argparse import ArgumentParser
 from hashlib import sha256, sha512
 from base64 import b64encode
+from random import seed, randint, choice
 
 def parse_args() -> tuple:
     """Parses command line arguments."""
@@ -51,11 +52,23 @@ def hash(algorithm: int, data: Any) -> str:
     Hashes encoded data.
     :param algorithm: Which SHA-2 algorithm to use
     :param data: The data to hash
-    :return: The hexadecimal string representatin of the hashed data
+    :return: The hexadecimal string representation of the hashed data
     """
 
     sha = sha512 if algorithm == 512 else sha256
     return sha(encode(data)).hexdigest()
+
+def insertSpecialChars(data: str) -> str:
+    """Inserts special characters pseudorandomly into a string."""
+
+    special_chars = ['?', '!', '@', '#', '$', '%']
+    seed(data)
+    num_chars = randint(1, int(len(data) / 2))
+    for _ in range(num_chars):
+        index = randint(0, len(data) - 1)
+        char = choice(special_chars)
+        data = ''.join([data[:index], char, data[index:]]) 
+    return data
 
 def generate_password(
     algorithm: int, service: str, secret: str, iteration: int
@@ -73,7 +86,9 @@ def generate_password(
     secret_h = hash(algorithm, secret)
     iteration_h = hash(algorithm, iteration)
     composite = ''.join([service_h, secret_h, iteration_h])
-    return hash(algorithm, composite)
+    composite_h = hash(algorithm, composite)
+    composite_b64str = encode(composite_h).decode()
+    return insertSpecialChars(composite_b64str)
 
 def main() -> None:
     """Runs the password manager program."""

dddab07158a1ef34d2f7427944baf9edf000dc98
thoresonjd <thoresonjd@gmail.com>
2024-01-03 23:08:00 -0800

    Removed driver code from password_manager.py and renamed it to password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,16 @@
 """
-File: password_manager.py
-Description: Generates passwords for different accounts
+File: password_generator.py
+Description: Handles password generation
 Author: Justin Thoreson
 Date: 3 January 2024
 """
 
-from argparse import ArgumentParser
 from hashlib import sha256
 from base64 import b64encode
 from random import seed, sample, choices, shuffle
 
-class PasswordManager(object):
-    """Manages password generation."""
+class PasswordGenerator(object):
+    """Handles password generation."""
 
     UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     LOWER = 'abcdefghijklmnopqrstuvwxyz'
@@ -23,11 +22,11 @@ class PasswordManager(object):
         """Seeds the pseudorandom generation."""
 
         composite = ''.join([service, secret, str(iteration)])
-        composite_h = PasswordManager.__hash(composite)
+        composite_h = PasswordGenerator.__hash(composite)
         seed(composite_h)
     
     @staticmethod
-    def generate_password(
+    def generate(
         min_length: int,
         include_upper: bool = True,
         include_lower: bool = True,
@@ -47,13 +46,13 @@ class PasswordManager(object):
         num_components = include_upper + include_lower + include_number + include_special
         if num_components <= 0:
             raise ValueError('Password must include at least one character type')
-        num_each = PasswordManager.__ceil_div(min_length, num_components)
-        uppers = PasswordManager.__sample(PasswordManager.UPPER, num_each) if include_upper else ''
-        lowers = PasswordManager.__sample(PasswordManager.LOWER, num_each) if include_lower else ''
-        numbers = PasswordManager.__sample(PasswordManager.NUMBER, num_each) if include_number else ''
-        specials = PasswordManager.__sample(PasswordManager.SPECIAL, num_each) if include_special else ''
+        num_each = PasswordGenerator.__ceil_div(min_length, num_components)
+        uppers = PasswordGenerator.__sample(PasswordGenerator.UPPER, num_each) if include_upper else ''
+        lowers = PasswordGenerator.__sample(PasswordGenerator.LOWER, num_each) if include_lower else ''
+        numbers = PasswordGenerator.__sample(PasswordGenerator.NUMBER, num_each) if include_number else ''
+        specials = PasswordGenerator.__sample(PasswordGenerator.SPECIAL, num_each) if include_special else ''
         composite = ''.join([uppers, lowers, numbers, specials])
-        return PasswordManager.__shuffle(composite)
+        return PasswordGenerator.__shuffle(composite)
 
     @staticmethod
     def __hash(data: str) -> str:
@@ -63,7 +62,7 @@ class PasswordManager(object):
         :return: The hexadecimal string representation of the hashed data
         """
 
-        return sha256(PasswordManager.__encode(data)).hexdigest()
+        return sha256(PasswordGenerator.__encode(data)).hexdigest()
 
     @staticmethod
     def __encode(data: str) -> bytes:
@@ -98,66 +97,3 @@ class PasswordManager(object):
         data_l = list(data)
         shuffle(data_l)
         return ''.join(data_l)
-
-def parse_args() -> tuple:
-    """Parses command line arguments."""
-    
-    arg_parser = ArgumentParser()
-    arg_parser.add_argument(
-        'service',
-        type=str,
-        help='the service in which to generate a password for'
-    )
-    arg_parser.add_argument(
-        'secret',
-        type=str,
-        help='a secret phrase'
-    )
-    arg_parser.add_argument(
-        'iteration',
-        type=int,
-        help='the number of times this password has been generated'
-    )
-    arg_parser.add_argument(
-        'min_length',
-        type=int,
-        help='the minimum length of the password'
-    )
-    arg_parser.add_argument(
-        '-u', '--upper',
-        action='store_true',
-        help='include uppercase letters'
-    )
-    arg_parser.add_argument(
-        '-l', '--lower',
-        action='store_true',
-        help='include lowercase letters'
-    )
-    arg_parser.add_argument(
-        '-n', '--number',
-        action='store_true',
-        help='include numbers'
-    )
-    arg_parser.add_argument(
-        '-s', '--special',
-        action='store_true',
-        help='include special characters'
-    )
-    return arg_parser.parse_args()
-
-def main() -> None:
-    """Runs the password manager program."""
-
-    args = parse_args()
-    password_manager = PasswordManager.seed(
-        args.service, args.secret, args.iteration)
-    try:
-        password = PasswordManager.generate_password(
-            args.min_length, args.upper, args.lower, args.number, args.special)
-    except ValueError as e:
-        print(e)
-    else:
-        print(password)
-
-if __name__ == '__main__':
-    main()

7bb9d8b445ddc9d996c388ab6d3bb08bfe9076ca
thoresonjd <thoresonjd@gmail.com>
2024-01-07 20:02:11 -0800

    Modified logger.py

--- logger.py
+++ logger.py
@@ -2,56 +2,66 @@
 File: logger.py
 Description: Logs messages to a file
 Author: Justin Thoreson
-Date: 3 January 2024
+Date: January 2024
 """
 
 class Logger(object):
     """Logs messages to a file."""
 
-    __slots__ = 'file', 'logs'
+    __slots__ = '__file', '__logs'
 
     def __init__(self, filename: str) -> None:
         """Opens/creates a log file and reads its contents."""
 
-        self.file = open(filename, 'a+')
-        self.logs = self.__read()
+        self.__file = open(filename, 'a+')
+        self.__logs = self.__read()
 
-    def log(self, message: str, end: str = '\n') -> None:
+    def log(self, message: str) -> None:
         """Logs a message."""
         
-        message = ''.join([message, end])
-        self.logs.append(message)
+        self.__logs.append(message)
         self.__write(message)
 
-    def log_if_not_exists(self, message: str, end: str = '\n') -> None:
-        """Logs a message only if it does not already exist in the logs."""
+    def log_if_not_exists(self, message: str) -> bool:
+        """
+        Logs a message only if it does not already exist in the logs.
+        :param message:
+        :return: True if the message was logged, False otherwise
+        """
         
-        if message not in self.logs:
-            self.log(message, end)
+        if message not in self.__logs:
+            self.log(message)
+            return True
+        return False
     
     def clear(self) -> None:
         """Clears all logs."""
 
-        self.logs.clear()
+        self.__logs.clear()
         self.__erase()
+    
+    def get_logs(self) -> list:
+        """Retrieves a copy of the current logs."""
+        
+        return self.__logs[:]
     
     def __read(self) -> list:
         """Reads all lines from the log file."""
 
-        self.file.seek(0)
-        return self.file.read().splitlines()
+        self.__file.seek(0)
+        return self.__file.read().splitlines()
     
-    def __write(self, message) -> None:
+    def __write(self, message: str) -> None:
         """Writes a message to the log file."""
 
-        self.file.write(message)
+        self.__file.write(f'{message}\n')
     
     def __erase(self) -> None:
         """Erases all contents of the log file."""
 
-        self.file.truncate(0)
+        self.__file.truncate(0)
 
     def __del__(self) -> None:
         """Closes the log file."""
 
-        self.file.close()
+        self.__file.close()
